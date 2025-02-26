2c270571c4f389a1d09e4f534a21691ea7810fa2
EriBloo <19932449+EriBloo@users.noreply.github.com>
2025-02-26 23:32:26 +0100

    add tests

---
+++ Tests/OptionTests.cs
@@ -0,0 +1,512 @@
+﻿using Options;
+using Results;
+
+namespace OptionsTests;
+
+internal static class OptionFactory
+{
+    public static Option<int> Some(int value = 1) => Option.Some(value);
+
+    public static Option<int> None() => Option.None();
+}
+
+[TestClass]
+public sealed class IsSomeTests
+{
+    [TestMethod]
+    public void Some_IsSome()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.IsTrue(option.IsSome());
+    }
+    [TestMethod]
+    public void None_Not_IsSome()
+    {
+        var option = OptionFactory.None();
+
+        Assert.IsFalse(option.IsSome());
+    }
+}
+
+[TestClass]
+public sealed class IsSomeAndTests
+{
+    [TestMethod]
+    public void Some_IsSomeAndTrue()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.IsTrue(option.IsSomeAnd(x => x == 1));
+    }
+    [TestMethod]
+    public void Some_Not_IsSomeAndFalse()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.IsFalse(option.IsSomeAnd(x => x == 2));
+    }
+    [TestMethod]
+    public void None_Not_IsSomeAndFalse()
+    {
+        var option = OptionFactory.None();
+
+        Assert.IsFalse(option.IsSomeAnd(x => x == 1));
+    }
+}
+
+[TestClass]
+public sealed class IsNoneTests
+{
+    [TestMethod]
+    public void None_IsNone()
+    {
+        var option = OptionFactory.None();
+
+        Assert.IsTrue(option.IsNone());
+    }
+    [TestMethod]
+    public void Some_Not_IsNone()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.IsFalse(option.IsNone());
+    }
+}
+
+[TestClass]
+public sealed class IsNoneOrTests
+{
+    [TestMethod]
+    public void None_IsNoneOrTrue()
+    {
+        var option = OptionFactory.None();
+
+        Assert.IsTrue(option.IsNoneOr(x => x == 1));
+    }
+    [TestMethod]
+    public void None_IsNoneOrFalse()
+    {
+        var option = OptionFactory.None();
+
+        Assert.IsTrue(option.IsNoneOr(x => x == 2));
+    }
+    [TestMethod]
+    public void Some_Not_IsNoneOrTrue()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.IsTrue(option.IsNoneOr(x => x == 1));
+    }
+    [TestMethod]
+    public void Some_Not_IsNoneOrFalse()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.IsFalse(option.IsNoneOr(x => x == 2));
+    }
+}
+
+[TestClass]
+public sealed class MapTests
+{
+    [TestMethod]
+    public void Some_Map()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(Option.Some(2), option.Map(x => x + 1));
+    }
+    [TestMethod]
+    public void None_Not_Map()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.Map(x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class MapOrTests
+{
+    [TestMethod]
+    public void Some_MapOr()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(2, option.MapOr(0, x => x + 1));
+    }
+    [TestMethod]
+    public void None_MapOr()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(0, option.MapOr(0, x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class MapOrElseTests
+{
+    [TestMethod]
+    public void Some_MapOrElse()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(2, option.MapOrElse(() => 0, x => x + 1));
+    }
+    [TestMethod]
+    public void None_MapOrElse()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(0, option.MapOrElse(() => 0, x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class InspectTests
+{
+    [TestMethod]
+    public void Some_Inspect()
+    {
+        var option = OptionFactory.Some();
+        var result = 0;
+
+        option.Inspect(x => result = x);
+
+        Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void None_Not_Inspect()
+    {
+        var option = OptionFactory.None();
+        var result = 0;
+
+        option.Inspect(x => result = x);
+
+        Assert.AreEqual(0, result);
+    }
+}
+
+[TestClass]
+public sealed class AndTests
+{
+    [TestMethod]
+    public void Some_AndSome()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.Some(2), option.And(other));
+    }
+    [TestMethod]
+    public void Some_AndNone()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.And(other));
+    }
+    [TestMethod]
+    public void None_AndSome()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.None(), option.And(other));
+    }
+    [TestMethod]
+    public void None_AndNone()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.And(other));
+    }
+}
+
+[TestClass]
+public sealed class AndThenTests
+{
+    [TestMethod]
+    public void Some_AndThenSome()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.Some(2), option.AndThen(x => other));
+    }
+    [TestMethod]
+    public void Some_AndThenNone()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.AndThen(x => other));
+    }
+    [TestMethod]
+    public void None_AndThenSome()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.None(), option.AndThen(x => other));
+    }
+    [TestMethod]
+    public void None_AndThenNone()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.AndThen(x => other));
+    }
+}
+
+[TestClass]
+public sealed class FilterTests
+{
+    [TestMethod]
+    public void Some_FilterTrue()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(Option.Some(1), option.Filter(x => x == 1));
+    }
+    [TestMethod]
+    public void Some_FilterFalse()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(Option.None(), option.Filter(x => x == 2));
+    }
+    [TestMethod]
+    public void None_Not_Filter()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.Filter(x => x == 1));
+    }
+}
+
+[TestClass]
+public sealed class OrTests
+{
+    [TestMethod]
+    public void Some_OrSome()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.Some(1), option.Or(other));
+    }
+    [TestMethod]
+    public void Some_OrNone()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.Some(1), option.Or(other));
+    }
+    [TestMethod]
+    public void None_OrSome()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.Some(2), option.Or(other));
+    }
+    [TestMethod]
+    public void None_OrNone()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.Or(other));
+    }
+}
+
+[TestClass]
+public sealed class OrElseTests
+{
+    [TestMethod]
+    public void Some_OrElseSome()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.Some(1), option.OrElse(() => other));
+    }
+    [TestMethod]
+    public void Some_OrElseNone()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.Some(1), option.OrElse(() => other));
+    }
+    [TestMethod]
+    public void None_OrElseSome()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.Some(2);
+
+        Assert.AreEqual(Option.Some(2), option.OrElse(() => other));
+    }
+    [TestMethod]
+    public void None_OrElseNone()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.None();
+
+        Assert.AreEqual(Option.None(), option.OrElse(() => other));
+    }
+}
+
+[TestClass]
+public sealed class XorTests
+{
+    [TestMethod]
+    public void Some_XorSome()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.Some(2);
+        Assert.AreEqual(Option.None(), option.Xor(other));
+    }
+    [TestMethod]
+    public void Some_XorNone()
+    {
+        var option = OptionFactory.Some();
+        var other = OptionFactory.None();
+        Assert.AreEqual(Option.Some(1), option.Xor(other));
+    }
+    [TestMethod]
+    public void None_XorSome()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.Some(2);
+        Assert.AreEqual(Option.Some(2), option.Xor(other));
+    }
+    [TestMethod]
+    public void None_XorNone()
+    {
+        var option = OptionFactory.None();
+        var other = OptionFactory.None();
+        Assert.AreEqual(Option.None(), option.Xor(other));
+    }
+}
+
+[TestClass]
+public sealed class UnwrapTests
+{
+    [TestMethod]
+    public void Some_Unwrap()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(1, option.Unwrap());
+    }
+    [TestMethod]
+    public void None_Throws()
+    {
+        var option = OptionFactory.None();
+
+        Assert.ThrowsException<InvalidOperationException>(() => option.Unwrap());
+    }
+}
+
+[TestClass]
+public sealed class ExpectTests
+{
+    [TestMethod]
+    public void Some_Expect()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(1, option.Expect("panic"));
+    }
+    [TestMethod]
+    public void None_Expect()
+    {
+        var option = OptionFactory.None();
+
+        Assert.ThrowsException<InvalidOperationException>(() => option.Expect("panic"));
+    }
+}
+
+[TestClass]
+public sealed class UnwrapOrTests
+{
+    [TestMethod]
+    public void Some_UnwrapOr()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(1, option.UnwrapOr(0));
+    }
+    [TestMethod]
+    public void None_UnwrapOr()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(0, option.UnwrapOr(0));
+    }
+}
+
+[TestClass]
+public sealed class UnwrapOrElseTests
+{
+    [TestMethod]
+    public void Some_UnwrapOrElse()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(1, option.UnwrapOrElse(() => 0));
+    }
+    [TestMethod]
+    public void None_UnwrapOrElse()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(0, option.UnwrapOrElse(() => 0));
+    }
+}
+
+[TestClass]
+public sealed class OkOrTests
+{
+    [TestMethod]
+    public void Some_OkOr()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(Result.Ok(1), option.OkOr(0));
+    }
+
+    [TestMethod]
+    public void None_OkOr()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(Result.Err(0), option.OkOr(0));
+    }
+}
+
+[TestClass]
+public sealed class OkOrElseTests
+{
+    [TestMethod]
+    public void Some_OkOrElse()
+    {
+        var option = OptionFactory.Some();
+
+        Assert.AreEqual(Result.Ok(1), option.OkOrElse(() => 0));
+    }
+    [TestMethod]
+    public void None_OkOrElse()
+    {
+        var option = OptionFactory.None();
+
+        Assert.AreEqual(Result.Err(0), option.OkOrElse(() => 0));
+    }
+}
---
+++ Tests/ResultTests.cs
@@ -0,0 +1,464 @@
+﻿using Options;
+using Results;
+
+namespace ResultTests;
+
+internal static class ResultFactory
+{
+    public static Result<int, int> Ok(int value = 1) => Result.Ok(value);
+
+    public static Result<int, int> Err(int error = 1) => Result.Err(error);
+}
+
+[TestClass]
+public sealed class IsOkTests
+{
+    [TestMethod]
+    public void Ok_IsOk()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.IsTrue(result.IsOk());
+    }
+
+    [TestMethod]
+    public void Err_Not_IsOk()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.IsFalse(result.IsOk());
+    }
+}
+
+[TestClass]
+public sealed class IsErrTests
+{
+    [TestMethod]
+    public void Err_IsErr()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.IsTrue(result.IsErr());
+    }
+
+    [TestMethod]
+    public void Ok_Not_IsErr()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.IsFalse(result.IsErr());
+    }
+}
+
+[TestClass]
+public sealed class IsOkAndTests
+{
+    [TestMethod]
+    public void Ok_IsOkAndTrue()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.IsTrue(result.IsOkAnd(x => x == 1));
+    }
+
+    [TestMethod]
+    public void Ok_IsOkAndFalse()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.IsFalse(result.IsOkAnd(x => x == 2));
+    }
+
+    [TestMethod]
+    public void Err_Not_IsOkAndTrue()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.IsFalse(result.IsOkAnd(x => x == 1));
+    }
+
+    [TestMethod]
+    public void Err_Not_IsOkAndFalse()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.IsFalse(result.IsOkAnd(x => x == 2));
+    }
+}
+
+[TestClass]
+public sealed class IsErrAndTests
+{
+    [TestMethod]
+    public void Err_IsErrAndTrue()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.IsTrue(result.IsErrAnd(x => x == 1));
+    }
+    [TestMethod]
+    public void Err_IsErrAndFalse()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.IsFalse(result.IsErrAnd(x => x == 2));
+    }
+    [TestMethod]
+    public void Ok_Not_IsErrAndTrue()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.IsFalse(result.IsErrAnd(x => x == 1));
+    }
+    [TestMethod]
+    public void Ok_Not_IsErrAndFalse()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.IsFalse(result.IsErrAnd(x => x == 2));
+    }
+}
+
+[TestClass]
+public sealed class OptionOkTests
+{
+    [TestMethod]
+    public void Ok_Some()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Option.Some(1), result.Ok());
+    }
+
+    [TestMethod]
+    public void Err_None()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Option.None(), result.Ok());
+    }
+}
+
+[TestClass]
+public sealed class OptionErrTests
+{
+    [TestMethod]
+    public void Err_Some()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Option.Some(1), result.Err());
+    }
+    [TestMethod]
+    public void Ok_None()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Option.None(), result.Err());
+    }
+}
+
+[TestClass]
+public sealed class MapTests
+{
+    [TestMethod]
+    public void Ok_Map()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Result.Ok(2), result.Map(x => x + 1));
+    }
+    [TestMethod]
+    public void Err_Map()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Result.Err(1), result.Map(x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class MapOrTests
+{
+    [TestMethod]
+    public void Ok_MapOr()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(2, result.MapOr(0, x => x + 1));
+    }
+    [TestMethod]
+    public void Err_MapOr()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(0, result.MapOr(0, x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class MapOrElseTests
+{
+    [TestMethod]
+    public void Ok_MapOrElse()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(2, result.MapOrElse(_ => 0, x => x + 1));
+    }
+    [TestMethod]
+    public void Err_MapOrElse()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(0, result.MapOrElse(_ => 0, x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class MapErrTests
+{
+    [TestMethod]
+    public void Ok_MapErr()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Result.Ok(1), result.MapErr(x => x + 1));
+    }
+    [TestMethod]
+    public void Err_MapErr()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Result.Err(2), result.MapErr(x => x + 1));
+    }
+}
+
+[TestClass]
+public sealed class InspectTests
+{
+    [TestMethod]
+    public void Ok_Inspect()
+    {
+        var result = ResultFactory.Ok();
+
+        result.Inspect(x => Assert.AreEqual(1, x));
+    }
+    [TestMethod]
+    public void Err_Inspect()
+    {
+        var result = ResultFactory.Err();
+
+        result.Inspect(x => throw new Exception("This should not throw"));
+    }
+}
+
+[TestClass]
+public sealed class InspectErrTests
+{
+    [TestMethod]
+    public void Ok_InspectErr()
+    {
+        var result = ResultFactory.Ok();
+
+        result.InspectErr(x => throw new Exception("This should not throw"));
+    }
+    [TestMethod]
+    public void Err_InspectErr()
+    {
+        var result = ResultFactory.Err();
+
+        result.InspectErr(x => Assert.AreEqual(1, x));
+    }
+}
+
+[TestClass]
+public sealed class ExpectTests
+{
+    [TestMethod]
+    public void Ok_Expect()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(1, result.Expect("This should not throw"));
+    }
+    [TestMethod]
+    public void Err_Expect()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.ThrowsException<Exception>(() => result.Expect("This should throw"));
+    }
+}
+
+[TestClass]
+public sealed class ExpectErrTests
+{
+    [TestMethod]
+    public void Ok_ExpectErr()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.ThrowsException<Exception>(() => result.ExpectErr("This should throw"));
+    }
+    [TestMethod]
+    public void Err_ExpectErr()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(1, result.ExpectErr("This should not throw"));
+    }
+}
+
+[TestClass]
+public sealed class UnwrapTests
+{
+    [TestMethod]
+    public void Ok_Unwrap()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(1, result.Unwrap());
+    }
+    [TestMethod]
+    public void Err_Unwrap()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.ThrowsException<Exception>(() => result.Unwrap());
+    }
+}
+
+[TestClass]
+public sealed class UnwrapErrTests
+{
+    [TestMethod]
+    public void Ok_UnwrapErr()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.ThrowsException<Exception>(() => result.UnwrapErr());
+    }
+    [TestMethod]
+    public void Err_UnwrapErr()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(1, result.UnwrapErr());
+    }
+}
+
+[TestClass]
+public sealed class UnwrapOrTests
+{
+    [TestMethod]
+    public void Ok_UnwrapOr()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(1, result.UnwrapOr(0));
+    }
+    [TestMethod]
+    public void Err_UnwrapOr()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(0, result.UnwrapOr(0));
+    }
+}
+
+[TestClass]
+public sealed class UnwrapOrElseTests
+{
+    [TestMethod]
+    public void Ok_UnwrapOrElse()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(1, result.UnwrapOrElse(_ => 0));
+    }
+    [TestMethod]
+    public void Err_UnwrapOrElse()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(0, result.UnwrapOrElse(_ => 0));
+    }
+}
+
+[TestClass]
+public sealed class OrElseTests
+{
+    [TestMethod]
+    public void Ok_OrElse()
+    {
+
+        var result = ResultFactory.Ok();
+        Assert.AreEqual(Result.Ok(1), result.OrElse<int>(_ => Result.Ok(0)));
+    }
+    [TestMethod]
+    public void Err_OrElse()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Result.Ok(0), result.OrElse<int>(_ => Result.Ok(0)));
+    }
+}
+
+[TestClass]
+public sealed class AndTests
+{
+    [TestMethod]
+    public void Ok_And()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Result.Ok(0), result.And<int>(Result.Ok(0)));
+    }
+    [TestMethod]
+    public void Err_And()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Result.Err(1), result.And<int>(Result.Ok(0)));
+    }
+}
+
+[TestClass]
+
+public sealed class AndThenTests
+{
+    [TestMethod]
+    public void Ok_AndThen()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Result.Ok(0), result.AndThen<int>(_ => Result.Ok(0)));
+    }
+    [TestMethod]
+    public void Err_AndThen()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Result.Err(1), result.AndThen<int>(_ => Result.Ok(0)));
+    }
+}
+
+[TestClass]
+public sealed class OrTests
+{
+    [TestMethod]
+    public void Ok_Or()
+    {
+        var result = ResultFactory.Ok();
+
+        Assert.AreEqual(Result.Ok(1), result.Or<int>(Result.Ok(0)));
+    }
+    [TestMethod]
+    public void Err_Or()
+    {
+        var result = ResultFactory.Err();
+
+        Assert.AreEqual(Result.Ok(0), result.Or<int>(Result.Ok(0)));
+    }
+}
